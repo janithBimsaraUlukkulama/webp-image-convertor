fd79dc3e60e88367bccdfb479b8dd6eea9df8765
bimsara <jbimsaraulukkulama@outlook.com>
2022-09-05 16:53:41 +0530

    convert to webp

--- resizeImage.js
+++ resizeImage.js
@@ -1,13 +1,11 @@
+// https://sharp.pixelplumbing.com/api-output#webp
 const sharp = require("sharp");
 
 async function resizeImage() {
     try {
         await sharp("sammy.png")
-            .resize({
-                width: 150,
-                height: 97
-            })
-            .toFile("sammy-resized.png");
+            .webp()
+            .toFile("sammy-resized.webp");
     } catch (error) {
         console.log(error);
     }
